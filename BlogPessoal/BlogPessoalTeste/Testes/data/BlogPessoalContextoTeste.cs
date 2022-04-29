2509a38871eadf9fcadfea93a09ffd62a7f12d7f
grillogui <grilloalencar@gmail.com>
2022-04-29 10:01:55 -0300

    Primeiro Teste

---
+++ BlogPessoal/BlogPessoalTeste/Testes/data/BlogPessoalContextoTeste.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using BlogPessoal.src.data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlogPessoal.src.modelos;
+using System.Linq;
+
+namespace BlogPessoalTeste.Testes.data
+{
+    [TestClass]
+    public class BlogPessoalContextoTeste
+    {
+        private BlogPessoalContexto _contexto;
+
+        [TestInitialize]
+        public void inicio()
+        {
+            var opt = new DbContextOptionsBuilder<BlogPessoalContexto>()
+                .UseInMemoryDatabase(databaseName: "db_blogpessoal")
+                .Options;
+
+            _contexto = new BlogPessoalContexto(opt);
+        }
+
+        [TestMethod]
+        public void InserirNovoUsuarioNoBancoRetornaUsuario()
+        {
+            UsuarioModelo Usuario = new UsuarioModelo();
+
+            Usuario.Nome = "Caroline Polimeno";
+            Usuario.Email = "cabs26@email.com";
+            Usuario.Senha = "050500";
+            Usuario.Foto = "LINKDAFOTO";
+
+            _contexto.Usuarios.Add(Usuario);
+
+            _contexto.SaveChanges();
+
+            Assert.IsNotNull(_contexto.Usuarios.FirstOrDefault(u => u.Email == "cabs26@email.com"));
+        }
+    }
+}
