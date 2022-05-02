a55f8840c0b5bf2868c439b03938eba74f0ba213
grillogui <grilloalencar@gmail.com>
2022-05-02 14:39:14 -0300

    ajustando documentação

--- BlogPessoal/BlogPessoal/src/repositorios/IPostagem.cs
+++ BlogPessoal/BlogPessoal/src/repositorios/IPostagem.cs
@@ -4,15 +4,15 @@ using System.Collections.Generic;
 
 namespace BlogPessoal.src.repositorios
 {
+    /// <summary>
+    /// <para>Resumo: Responsavvel por representar ações de CRUD de postagem</para>
+    /// <para>Criado por: Guilherme Grillo</para>
+    /// <para>versão: 1.0</para>
+    /// <para>Data: 29/04/2022</para>
+    /// </summary>
+    /// 
     public interface IPostagem
     {
-        /// <summary>
-        /// <para>Resumo: Responsavvel por representar ações de CRUD de postagem</para>
-        /// <para>Criado por: Guilherme Grillo</para>
-        /// <para>versão: 1.0</para>
-        /// <para>Data: 29/04/2022</para>
-        /// </summary>
-
         public interface IPostagem
         {
             void NovaPostagem(NovaPostagemDTO postagem);
